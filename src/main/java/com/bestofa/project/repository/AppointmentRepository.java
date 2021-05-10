c8c2e68cc57b752cc8ae1f7136449941328e6962
Alperen <aalperenelbasan@gmail.com>
2021-05-10 17:38:54 -0500

    fixed issues

--- src/main/java/com/bestofa/project/repository/AppointmentRepository.java
+++ src/main/java/com/bestofa/project/repository/AppointmentRepository.java
@@ -3,6 +3,7 @@ package com.bestofa.project.repository;
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bestofa.project.domain.Appointment;
 
